9bc866c42547024c3103ce68c15711368097bc8a
System User <cs421g28@comp421.CS.McGill.CA>
2019-02-28 14:56:04 -0500

    added primary key

--- generate-tables.sql
+++ generate-tables.sql
@@ -1,2 +1,2 @@
 CREATE TABLE Repairtypes(repair_name VARCHAR(30) PRIMARY KEY, description VARCHAR(200));
-CREATE TABLE Truckmodels(model_id INT, model_name VARCHAR(20), manufacturer VARCHAR(20));
+CREATE TABLE Truckmodels(model_id INT PRIMARY KEY, model_name VARCHAR(20), manufacturer VARCHAR(20));
