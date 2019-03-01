5d9d8711d73ae78e0528182b5478088c72cb787b
Maxime Plante <maxime.pl@outlook.com>
2019-03-01 18:37:01 -0500

    Moar fixing

--- project2/part5.sql
+++ project2/part5.sql
@@ -8,7 +8,7 @@ GROUP BY A.appointment_id;
 --Display all trucks that require repair
 SELECT DISTINCT T.licence_plate, TM.model_name, TM.manufacture
 FROM Trucks AS T
-INNER JOIN Trucksmodels AS TM
+INNER JOIN truckmodels AS TM
 ON T.model_id=TM.model_id
 INNER JOIN Appointments AS A
 ON T.licence_plate=A.licence_plate
@@ -17,7 +17,7 @@ ON T.model_id=RF.model_id
 WHERE A.date <= current_date AND ( current_date - A.date > RF.frequency );
 
 --Display list of salesman in decressing order of total rental sales
-SELECT DISTINCT E.employee_id, E.employeename, SUM(R.price) AS total_sales
+SELECT DISTINCT E.employee_id, E.employee_name, SUM(R.price) AS total_sales
 FROM Employees AS E
 INNER JOIN Rentals AS R
 ON E.employee_id=R.employee_id
@@ -27,7 +27,7 @@ ORDER BY total_sales DESC;
 --Diplay all possible kinds of repairs (type and truck model) and the average cost
 SELECT DISTINCT TM.model_id, TM.model_name, TM.manufacture, RT.repair_name,
 COALESCE(AVG(R.cost), 0) AS avg_cost
-FROM Trucksmodels AS TM
+FROM truckmodels AS TM
 INNER JOIN Repairfrequency AS RF
 ON TM.model_id=RF.model_id
 LEFT JOIN Repairs AS R
@@ -35,7 +35,7 @@ ON RF.repair_name=R.repair_name
 GROUP BY TM.model;
 
 --Display list of employees sorted by latest activity (latest completed appointment/rental sale)
-SELECT DISTINCT E.employee_id, E.employeename,
+SELECT DISTINCT E.employee_id, E.employee_name,
 COALESCE(MAX(R.start_date)) AS latest_activity
 FROM Employees AS E
 INNER JOIN Salesmen AS S
@@ -44,7 +44,7 @@ LEFT JOIN Rentals AS R
 ON E.employee_id=R.employee_id
 GROUP BY E.employee_id
 UNION
-SELECT DISTINCT E.employee_id, E.employeename,
+SELECT DISTINCT E.employee_id, E.employee_name,
 COALESCE(MAX(A.date)) AS latest_activity
 FROM Employees AS E
 INNER JOIN Mechanics AS M
