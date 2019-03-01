7662d8ceadd4a7fea2a979e1ff6a1aba60b4d512
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2019-02-28 21:30:58 -0500

    part 3

---
+++ project2/part3.sql
@@ -0,0 +1,5 @@
+INSERT INTO Repairtypes VALUES ('engine maintenance', 'thermocapacitor is broken, need to re-attach frame');
+INSERT INTO Truckmodels VALUES (123431343, 'F-150', 'FORD');
+INSERT INTO Rentees VALUES (4569381, 'Mongo Tremblay', '12 Grimmauld Place');
+INSERT INTO Trucks VALUES ('H70KAF', 'black', 123431343);
+INSERT INTO Trucks VALUES ('H40KAF', 'black', 123431343);
