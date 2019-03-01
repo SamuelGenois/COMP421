c379604539bd03215a23eab4cd4b27b96f436f55
System User <cs421g28@comp421.CS.McGill.CA>
2019-02-28 20:25:59 -0500

    Debugged project2.bash

--- project2/part5.sql
+++ project2/part5.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT A.appointment_id, "date", SUM(R.cost) AS total_cost
 FROM Appointments AS A
 INNER JOIN Repairs AS R
 ON A.appointment_id=R.appointment_id
-GROUP BY A.appointment_id
+GROUP BY A.appointment_id;
 
 --Display all trucks that require repair
 SELECT DISTINCT T.licence_plate, TM.model_name, TM.manufacture
@@ -16,7 +16,7 @@ INNER JOIN Repairfrequency AS RF
 ON T.model_id=RF.model_id
 WHERE A.date <= current_date
 GROUP BY T.licence_plate
-HAVING ANY ( current_date-A.date > RF.frequency )
+HAVING ANY ( current_date-A.date > RF.frequency );
 
 --Display list of salesman in decressing order of total rental sales
 SELECT DISTINCT E.employee_id, E.employeename, SUM(R.price) AS total_sales
@@ -24,7 +24,7 @@ FROM Employees AS E
 INNER JOIN Rentals AS R
 ON E.employee_id=R.employee_id
 GROUP BY E.employee_id
-ORDER BY total_sales DESC
+ORDER BY total_sales DESC;
 
 --Diplay all possible kinds of repairs (type and truck model) and the average cost
 SELECT DISTINCT TM.model_id, TM.model_name, TM.manufacture, RT.repair_name,
@@ -34,7 +34,7 @@ INNER JOIN Repairfrequency AS RF
 ON TM.model_id=RF.model_id
 LEFT JOIN Repairs AS R
 ON RF.repair_name=R.repair_name
-GROUP BY TM.model
+GROUP BY TM.model;
 
 --Display list of employees sorted by latest activity (latest completed appointment/rental sale)
 SELECT DISTINCT E.employee_id, E.employeename,
@@ -54,4 +54,4 @@ ON E.employee_id=M.employee_id
 LEFT JOIN Appointments AS A
 ON E.employee_id=A.employee_id
 GROUP BY E.employee_id
-ORDER BY latest_activity DESC
+ORDER BY latest_activity DESC;
--- project2/project2.bash
+++ project2/project2.bash
@@ -1,18 +1,22 @@
 #/bin/bash
 
-$HEADER_FILE="./header.txt"
-$OUTPUT_FILE="./project2.txt"
+HEADER_FILE="./header.txt"
+OUTPUT_FILE="./project2.txt"
 
-rm $OUTPUT_FILE
+if [[ -e $OUTPUT_FILE ]]; then
+    rm $OUTPUT_FILE
+fi
+
 cat $HEADER_FILE > $OUTPUT_FILE
 
-psql -w -f setup.sql
+psql -w -f setup.sql cs421
 
 for i in {2..9}; do
-    if [[ -r part$1.sql ]]; then
+    SOURCE_FILE="part$i.sql" 
+    if [[ -f $SOURCE_FILE ]]; then
         echo "\n$1.\n\nSource:\n" > $OUTPUT_FILE
-        cat part$1.sql
-        echo "\nOutput:\n"
-        psql -w -f part$1.sql > $OUTPUT_FILE
+        cat $SOURCE_FILE > $OUTPUT_FILE
+        echo "\nOutput:\n" > $OUTPUT_FILE
+        psql -w -f $SOURCE_FILE cs421 > $OUTPUT_FILE
     fi
 done
