f9849600a00cae07abb9186acf722cfc50282f2e
Samuel Genois <samuel.genois@mail.mcgill.ca>
2019-02-28 19:14:15 -0500

    Completed part5

--- project2/project2.bash
+++ project2/project2.bash
@@ -9,9 +9,10 @@ cat $HEADER_FILE > $OUTPUT_FILE
 psql -w -f setup.sql
 
 for i in {2..9}; do
-    echo "\n$1.\n\nSource:\n" > $OUTPUT_FILE
-    cat part$1.sql
-    echo "\nOutput:\n"
-    psql -w -f part$1.sql > $OUTPUT_FILE
+    if [[ -r part$1.sql ]]; then
+        echo "\n$1.\n\nSource:\n" > $OUTPUT_FILE
+        cat part$1.sql
+        echo "\nOutput:\n"
+        psql -w -f part$1.sql > $OUTPUT_FILE
+    fi
 done
-
