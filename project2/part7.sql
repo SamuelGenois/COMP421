7891980ce010c2ce01e530c7caad17b748003c1a
Maxence Frenette <maxence.frenette@gmail.com>
2019-03-02 09:07:23 -0500

    Clean up part 7.1

--- project2/part7.sql
+++ project2/part7.sql
@@ -1,7 +1,7 @@
--- List the experience that a given mechanic has with working with specific truck models
+-- Lists the experience that a given mechanic has with working with specific truck models
 -- This can be useful to determine which mechanics to assign to which trucks
 
--- CREATE VIEW MECHANICS_EXPERIENCE as
+CREATE VIEW MECHANICS_EXPERIENCE as
 SELECT
     employees.employee_id,
     employees.employee_name,
@@ -16,4 +16,8 @@ INNER JOIN trucks
 ON appointments.license_plate = trucks.license_plate
 INNER JOIN truckmodels
 ON trucks.model_id = truckmodels.model_id
-GROUP BY employees.employee_id, employees.employee_name, truckmodels.model_id, truckmodels.model_name, truckmodels.manufacturer
+GROUP BY employees.employee_id, employees.employee_name, truckmodels.model_id, truckmodels.model_name, truckmodels.manufacturer;
+
+-- Since the role of this view is simply to create a report, the most interesting thing to do is simply to query it
+-- Note that we cannot insert into this view because it utilises aggregate functions and a GROUP BY clause
+SELECT * from MECHANICS_EXPERIENCE
